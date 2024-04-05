3105e3e8bb46c6fbaa4820615c6861ed06912fb7
Marcelo Fortuna <celofortuna@gmail.com>
2024-04-04 21:39:06 -0300

    feat: Creating new route "GetById" for events.

--- PassIn.Api/Controllers/EventsController.cs
+++ PassIn.Api/Controllers/EventsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PassIn.Application.UseCases.Events.GetById;
 using PassIn.Application.UseCases.Events.Register;
 using PassIn.Communication.Requests;
 using PassIn.Communication.Responses;
@@ -32,5 +33,30 @@ namespace PassIn.Api.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, new ResponseErrorJson("Unknown error."));
             }
         }
+
+        [HttpGet]
+        [Route("{id}")]
+        [ProducesResponseType(typeof(ResponseEventJson), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status404NotFound)]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            try
+            {
+                var useCase = new GetEventByIdUseCase();
+
+                var response = useCase.Execute(id);
+
+                return Ok(response);
+            }
+            catch (PassInException ex)
+            {
+                return NotFound(new ResponseErrorJson(ex.Message));
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseErrorJson("Unknown error."));
+            }
+        }
+
     }
 }
